780953436f2ea2cbcbd9d84eb6cb037161ad7146
sehrishnadeem665 <sehrishnadeem665@gmail.com>
2026-07-06 16:57:20 +0500

    Disable Next image optimization to serve images from /public on static hosts

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    // Disable built-in image optimization so images are served directly
+    // This helps static hosts that don't support Next's image optimizer
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
